a0dc875e7cfac220094e3a58114d3ea5fd2c643e
Dimitri Molenaars <Tyrope@TyRope.nl>
2014-07-13 22:48:15 +0200

    [Timer]Using config where it makes sense.

--- LRB-Timer.py
+++ LRB-Timer.py
@@ -16,13 +16,25 @@ def setup(bot):
     bot.memory['timer']['index'] = 0
     bot.memory['timer']['enabled'] = True
 
-    bot.memory['timer']['channel'] = '#tyrope' # The twitch channel you want this module to run in.
-    bot.memory['timer']['ctt_default'] = 'kiRU4' # Default click to tweet ID
     bot.memory['timer']['lines'] = [ # The bot will cycle through these.
         'Share the love! Click here to tweet the stream. http://ctt.ec/%s', #CTT messages need %s to insert the ID.
         'Do you like roleplaying? We do a weekly Pathfinder series called "Pathfinders found" on Thursdays. All previous episodes (except ep3, sorry) are available on my profile.',
         'If you\'re enjoying the stream, please give me a follow. It helps a lot on my road to partnership. Thanks!'
         ]
+
+def configure(config):
+    """
+| [LRB] | example | purpose |
+| -------- | ------- | ------- |
+| channel | #LionRoarBot | The channel to spam these messages in. |
+| ctt_default | kiRU4 | The default click to tweet URL ID. |
+"""
+    chunk = ''
+    if config.option('Configuring LRB Timer module', False):
+        config.interactive_add('LRB', 'channel', 'channel', '')
+        config.interactive_add('LRB', 'ctt_default', 'ctt-default', '')
+    return chunk
+
 
 @interval(900)
 def timed_message(bot):
@@ -39,7 +51,7 @@ def timed_message(bot):
     # CTT thing.
     if 'http://ctt.ec/' in msg:
         if 'ctt' not in bot.memory['timer']:
-            bot.memory['timer']['ctt'] = bot.memory['timer']['ctt_default']
+            bot.memory['timer']['ctt'] = bot.config.timer.ctt_default
         msg = msg % bot.memory['timer']['ctt']
 
     # Move index up one, or loop.
@@ -48,7 +60,7 @@ def timed_message(bot):
         bot.memory['timer']['index'] = 0
 
     # Say!
-    bot.msg(bot.memory['timer']['channel'], msg)
+    bot.msg(bot.memory.timer.channel, msg)
 
 @commands('ctt')
 def ctt(bot, trigger):
@@ -59,7 +71,7 @@ def ctt(bot, trigger):
         bot.memory['timer']['ctt'] = trigger.group(2)
         bot.reply('Updated click to tweet link to http://ctt.ec/%s' % trigger.group(2))
     else:
-        bot.memory['timer']['ctt'] = 'kiRU4'
+        bot.memory['timer']['ctt'] = bot.config.timer.ctt_default
         bot.reply('Reset click to tweet link to default.')
 
 @commands('spam')
