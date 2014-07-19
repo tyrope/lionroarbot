e750b4db644c871ea3523e0a585765199e34056f
Dimitri Molenaars <Tyrope@TyRope.nl>
2014-07-19 22:41:03 +0200

    [Core]Regulars.

--- LRB-Core.py
+++ LRB-Core.py
@@ -8,6 +8,46 @@ These modules are built on top of the Willie system.
 http://willie.dftba.net/
 """
 
-# TODO
-# List of channel regulars. (Config)
+from willie.config import ConfigurationError
+from willie.module import commands, OP
+from sqlite3 import OperationalError # TODO: Fix this if/when WillieDB abstracts
+
+def setup(bot):
+    table_layout = ['nick']
+
+    if not bot.db:
+        raise ConfigurationError("No database configured.")
+    if not bot.db.lrb_regulars:
+        # 404 - Table not found.
+        bot.db.add_table('lrb_regulars', table_layout, 'nick')
+
+    for col in table_layout:
+        # Just in case not all columns are present.
+        try:
+            bot.db.lrb_regulars.add_columns([col])
+        except OperationalError as e:
+            # TODO: Fix this if/when WillieDB abstracts
+            pass
+
+@commands('reg','regular')
+def regular(bot, trigger):
+    if bot.privileges[trigger.sender][trigger.nick] < OP:
+        return bot.reply("Only moderators can alter regulars.")
+
+    if not trigger.group(3):
+        return bot.reply("Do what?")
+    if not trigger.group(4):
+        return bot.reply("To who?")
+
+    cmd = trigger.group(3).lower()
+    target = trigger.group(4).lower()
+
+    if cmd == 'add':
+        bot.db.lrb_regulars.update(target, {})
+        return bot.reply('%s added to regulars list.' % (target,))
+    elif cmd == 'del':
+        bot.db.lrb_regulars.delete(target)
+        return bot.reply('%s removed from regulars list.' % (target,))
+    else:
+        return bot.reply("Unknown parameter. Valid commands: !reg add, !reg del")
 
