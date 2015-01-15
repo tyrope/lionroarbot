6d91be476867f5c6d7535af400e13f4a5570bf30
Dimitri Molenaars <Tyrope@TyRope.nl>
2015-01-15 15:35:01 +0100

    [Core]Convert to Willie 5.0

--- LRB-Core.py
+++ LRB-Core.py
@@ -12,18 +12,8 @@ from willie.config import ConfigurationError
 from willie.module import commands, OP
 
 def setup(bot):
-    table_layout = ['nick']
-
-    if not bot.db:
-        raise ConfigurationError("No database configured.")
-    if not bot.db.lrb_regulars:
-        # 404 - Table not found.
-        bot.db.add_table('lrb_regulars', table_layout, 'nick')
-
-    for col in table_layout:
-        # Just in case not all columns are present.
-        if not bot.db.lrb_regulars.has_columns(col):
-            bot.db.lrb_regulars.add_columns([col])
+    bot.db.execute('CREATE TABLE IF NOT EXISTS lrb_regulars '+
+        '(nick STRING, PRIMARY KEY (nick))')
 
 @commands('reg','regular')
 def regular(bot, trigger):
@@ -42,10 +32,10 @@ def regular(bot, trigger):
     target = trigger.group(4).lower()
 
     if cmd == 'add':
-        bot.db.lrb_regulars.update(target, {})
+        bot.db.execute('INSERT INTO lrb_regulars (nick) VALUES (?)', (target,))
         return bot.reply('%s added to regulars list.' % (target,))
     elif cmd == 'del':
-        bot.db.lrb_regulars.delete(target)
+        bot.db.execute('DELETE FROM lrb_regulars WHERE nick=?', (target,))
         return bot.reply('%s removed from regulars list.' % (target,))
     else:
         return bot.reply("Unknown parameter. Valid commands: !reg add, !reg del")
