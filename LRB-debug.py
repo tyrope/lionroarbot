b057c356bb087d24e6f519b8b3d9530f3d47c182
Dimitri Molenaars <Tyrope@TyRope.nl>
2015-06-26 17:56:18 +0200

    [Debug]Add modified debug module from embolalia/willie-extras

---
+++ LRB-debug.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+"""
+LRB-debug.py - LRB Debugging Module - Based on Willie's Debug module.
+Copyright 2013, Dimitri "Tyrope" Molenaars, Tyrope.nl
+Licensed under the Eiffel Forum License 2.
+
+http://willie.dftba.net
+"""
+
+from willie.module import commands, example, require_admin
+
+@require_admin
+@commands('privs')
+@example('.privs', '.privs #channel')
+def privileges(bot, trigger):
+    """Print the privileges of a specific channel, or the entire array."""
+    if trigger.group(2):
+        try:
+            bot.say(str(bot.privileges[trigger.group(2)]))
+        except Exception:
+            bot.say("Channel not found.")
+    else:
+        bot.say(str(bot.privileges))
+
