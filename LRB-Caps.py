5928df04662386d1e088f36c59e7fc040fab4654
Dimitri Molenaars <Tyrope@TyRope.nl>
2015-09-10 00:56:15 +0200

    [Caps]Redefine the caps limit to ignore punctuation, inform people better of what they get told off for. Attempt 1 at trying to implement regulars.

--- LRB-Caps.py
+++ LRB-Caps.py
@@ -10,6 +10,11 @@ http://sopel.chat/
 
 from collections import Counter
 from sopel.module import rule, NOLIMIT
+try:
+    from sopel.modules import LRB-Core as core
+    core_available = True
+except ImportError:
+    core_available = False
 
 @rule('.*')
 def caps_detection(bot, trigger):
@@ -18,23 +23,35 @@ def caps_detection(bot, trigger):
     """
     try:
         if trigger.admin:
+            # This person is a moderator.
             return NOLIMIT
     except KeyError as e:
         # This potentially lets new-joiners shout for a little bit.
         # #blametwitch
         return NOLIMIT
 
+    if core_available:
+        #We can check for regulars.
+        if(core.isReg(trigger.sender, trigger.nick)):
+            #This person is a regular.
+            return NOLIMIT
+
     if len(trigger.group(0)) < 10:
+        # This message was very short, could be something like "OMG",
+        # let's not make a fuss about it.
         return NOLIMIT
 
     counter = Counter(trigger.group(0))
-    c = 0
-    for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-        c += counter[char]
+    caps = 0
+    lowercase = 0
+    for c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        caps += counter[c]
+    for c in "abcdefghijklmnopqrstuvwxyz":
+        lowercase += counter[c]
 
-    if c*2 >= len(trigger.group(0).replace(' ','')):
+    if caps >= lowercase:
         bot.say('.ban '+trigger.nick)
-        bot.reply('Oi! I\'m the only one allowed to ROAAAAAAAAAAAAR around here!')
+        bot.reply('Oi! I\'m the only one allowed to ROAAAAAAAAAAAAR around here! (Caps purge)')
         bot.say('.unban '+trigger.nick)
     else:
         return NOLIMIT
