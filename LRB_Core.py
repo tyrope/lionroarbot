f3405cd12067f905b76b5edc1815e529303222ea
Dimitri Molenaars <Tyrope@TyRope.nl>
2015-09-13 19:42:01 +0200

    [Core]Forgot a :

--- LRB_Core.py
+++ LRB_Core.py
@@ -65,7 +65,7 @@ def regular(bot, trigger):
         # TODO maybe, owner only?
         q = 'SELECT nick FROM lrb_regulars WHERE channel=?'
         regulars = ''
-        for reg in bot.db.execute(q, (trigger.sender,))
+        for reg in bot.db.execute(q, (trigger.sender,)):
             regulars += reg + ', '
         regulars = regulars[:-2] #remove the last ', '
         return bot.reply('Channel regulars: '+regulars)
